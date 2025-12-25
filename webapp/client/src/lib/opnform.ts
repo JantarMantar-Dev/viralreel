8b254c41846b59356db5aa446a4d8a8f4e8dadf7
Minal Viradia <minalviradia@Minals-MacBook-Pro.local>
2025-12-25 17:49:31 -0600

    feat: include authenticated user email in Contact Sales OpnForm submission

--- webapp/client/src/lib/opnform.ts
+++ webapp/client/src/lib/opnform.ts
@@ -38,6 +38,7 @@ export const CONTACT_SALES_FORM = {
         FULL_NAME: "82b7f193-edd2-4e24-88df-5961a0589075",
         COMPANY_NAME: "b6744ab1-aee6-4aee-8ec7-26c12af0b507",
         APP_NAME: "52a49bb3-3d01-4e6e-81c4-e83c9cc1d876",
+        APP_USER_ID: "72ceed4b-1ecc-4160-9bd1-3eff5622a2b9",
         WORK_EMAIL: "49cb1254-7882-4968-b058-d6e1a461d81a",
         MESSAGE: "d1c8fc41-6056-4553-acf8-0e1c00c19a1a",
     },
